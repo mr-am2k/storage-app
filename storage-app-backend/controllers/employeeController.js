014751ecb84ebe7b1878db3e2034ac141ccd66fb
Muamer Aličković <alickovicmuamer1@gmail.com>
2023-01-18 17:21:34 +0100

    Finished employee module

--- storage-app-backend/controllers/employeeController.js
+++ storage-app-backend/controllers/employeeController.js
@@ -21,12 +21,10 @@ const addEmployee = async (req, res) => {
     );
 
     if (userExists.rows[0].count > 0) {
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({
-          message: 'Employee already exists with email: ' + sentEmployee.email,
-          timestamp: new Date(),
-        });
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: 'Employee already exists with email: ' + sentEmployee.email,
+        timestamp: new Date(),
+      });
     }
 
     const usernameExists = await client.query(
@@ -35,13 +33,10 @@ const addEmployee = async (req, res) => {
     );
 
     if (usernameExists.rows[0].count > 0) {
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({
-          message:
-            'User already exists with username: ' + sentEmployee.username,
-          timestamp: new Date(),
-        });
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: 'User already exists with username: ' + sentEmployee.username,
+        timestamp: new Date(),
+      });
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -64,7 +59,12 @@ const addEmployee = async (req, res) => {
 
     const newUser = await client.query(
       'INSERT INTO users (employee_id, username, password, role) VALUES ($1, $2, $3, $4) RETURNING *',
-      [employeeResponse.id, sentEmployee.username, employeePassword, ROLES_LIST.User]
+      [
+        employeeResponse.id,
+        sentEmployee.username,
+        employeePassword,
+        ROLES_LIST.User,
+      ]
     );
 
     const userResponse = newUser.rows[0];
@@ -77,4 +77,97 @@ const addEmployee = async (req, res) => {
   }
 };
 
-module.exports = { addEmployee };
+const getEmployees = async (req, res) => {
+  try {
+    const fetchedEmployees = await client.query(
+      'SELECT * FROM employees e INNER JOIN users u ON u.employee_id = e.id'
+    );
+
+    const listOfEmployees = [...fetchedEmployees.rows];
+
+    res.status(StatusCodes.OK).json({ listOfEmployees });
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: error });
+  }
+};
+
+const getEmployee = async (req, res) => {
+  const employeeId = req.params.employeeId;
+
+  try {
+    const fetchedEmployees = await client.query(
+      'SELECT * FROM employees e INNER JOIN users u ON u.employee_id = e.id WHERE id = $1',
+      [employeeId]
+    );
+
+    const requestedEmployee = fetchedEmployees.rows[0];
+
+    if (!requestedEmployee) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: 'There is no user with id: ' + employeeId });
+    }
+
+    res.status(StatusCodes.OK).json({ requestedEmployee });
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: error });
+  }
+};
+
+const updateEmployee = async (req, res) => {
+  const employeeId = req.params.employeeId;
+
+  try {
+    const fetchedEmployees = await client.query(
+      'SELECT * FROM employees e INNER JOIN users u ON u.employee_id = e.id WHERE id = $1',
+      [employeeId]
+    );
+
+    const requestedEmployee = fetchedEmployees.rows[0];
+
+    if (!requestedEmployee) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: 'There is no user with id: ' + employeeId });
+    }
+
+    const updateData = req.body;
+
+    let userPassword = requestedEmployee.password;
+
+    if (!(await bcrypt.compare(updateData.password, userPassword))) {
+      const salt = await bcrypt.genSalt(10);
+
+      userPassword = await bcrypt.hash(updateData.password, salt);
+    }
+
+    const updatedEmployee = await client.query(
+      'UPDATE employees SET first_name = $1, last_name = $2, phone_number = $3, address = $4, email = $5, employment_date = $6, dismissal_date = $7 WHERE id = $8  RETURNING *;',
+      [
+        updateData.firstName,
+        updateData.lastName,
+        updateData.phoneNumber,
+        updateData.address,
+        updateData.email,
+        updateData.employmentDate,
+        updateData.dismissalDate,
+        employeeId,
+      ]
+    );
+
+    const updatedUser = await client.query(
+      'UPDATE users SET username = $1, password = $2 WHERE employee_id = $3 RETURNING *;',
+      [updateData.username, userPassword, employeeId]
+    );
+
+    const response = {
+      employee: updatedEmployee.rows[0],
+      user: updatedUser.rows[0],
+    };
+    res.status(StatusCodes.OK).json({ response });
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: error });
+  }
+};
+
+module.exports = { addEmployee, getEmployees, getEmployee, updateEmployee };
--- storage-app-backend/routes/employeeRoute.js
+++ storage-app-backend/routes/employeeRoute.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { addEmployee } = require('../controllers/employeeController.js');
+const {
+  addEmployee,
+  getEmployees,
+  getEmployee,
+  updateEmployee
+} = require('../controllers/employeeController.js');
 const { body } = require('express-validator');
 const verifyJWT = require('../middleware/authMiddleware.js');
 const ROLES_LIST = require('../config/roles_list.js');
@@ -7,9 +12,66 @@ const verifyRole = require('../middleware/roleMiddleware.js');
 
 const router = express.Router();
 
+router.route('/').get(verifyJWT, verifyRole(ROLES_LIST.Admin), getEmployees);
+
+router
+  .route('/:employeeId')
+  .get(verifyJWT, verifyRole(ROLES_LIST.Admin), getEmployee);
+
+router.route('/update/:employeeId').put(
+  verifyJWT,
+  verifyRole(ROLES_LIST.Admin),
+  body('firstName')
+    .not()
+    .isEmpty()
+    .trim()
+    .withMessage('First name is required'),
+  body('lastName').not().isEmpty().trim().withMessage('Last name is required'),
+  body('phoneNumber')
+    .notEmpty()
+    .withMessage('Phone number is required')
+    .isNumeric()
+    .withMessage('Phone number must be numeric')
+    .isLength({ min: 5, max: 15 })
+    .withMessage('Phone number must be between 5 and 15 digits'),
+  body('address').not().isEmpty().trim().withMessage('Address is required'),
+  body('email')
+    .not()
+    .isEmpty()
+    .trim()
+    .withMessage('Email is required')
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('Email needs to be in format user@domain.com'),
+  body('employmentDate')
+    .not()
+    .isEmpty()
+    .trim()
+    .withMessage('Employment date is required'),
+  body('username')
+    .not()
+    .isEmpty()
+    .trim()
+    .withMessage('Username is required')
+    .isLength({ min: 4, max: 15 })
+    .withMessage('Username must be between 4 and 15 characters'),
+  body('password')
+    .not()
+    .isEmpty()
+    .trim()
+    .withMessage('Password is required')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, 'i')
+    .withMessage(
+      'Password must contain at least one lowercase letter, one uppercase letter and one number.'
+    ),
+  updateEmployee
+);
+
 router.route('/add').post(
   verifyJWT,
-  verifyRole(ROLES_LIST.User, ROLES_LIST.Admin),
+  verifyRole(ROLES_LIST.Admin),
   body('firstName')
     .not()
     .isEmpty()
